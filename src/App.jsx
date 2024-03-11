b0b38cc13414b169f1a52299f73fe96bc2c31870
martasvobo <62069655+martasvobo@users.noreply.github.com>
2024-03-11 21:47:38 +0200

    added react + vite project and initialized firebase

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAHMKdvge9IbE5Q1mocLk9u_t9ZOtDAet8",
+  authDomain: "ktu-calculator.firebaseapp.com",
+  projectId: "ktu-calculator",
+  storageBucket: "ktu-calculator.appspot.com",
+  messagingSenderId: "841434618819",
+  appId: "1:841434618819:web:73564581e91da712b03b76"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>ktu calculator</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
