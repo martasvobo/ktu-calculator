b6b31e5e8857ab17f1c38b428139c7090f43ef49
martasvobo <62069655+martasvobo@users.noreply.github.com>
2024-04-15 16:46:26 +0300

    added password reset

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -1,6 +1,9 @@
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import {
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import React, { useState } from "react";
-import { useNavigate, useNavigation } from "react-router-dom";
+import { auth } from "./firebase";
 
 export default function LoginPage() {
   const [email, setEmail] = useState("");
@@ -27,7 +30,6 @@ export default function LoginPage() {
       />
       <button
         onClick={() => {
-          const auth = getAuth();
           signInWithEmailAndPassword(auth, email, password)
             .then((userCredential) => {
               window.open("/", "_self");
@@ -38,6 +40,19 @@ export default function LoginPage() {
         }}
       >
         Log in
+      </button>
+      <button
+        onClick={() => {
+          sendPasswordResetEmail(auth, email)
+            .then(() => {
+              alert("Password reset email sent!");
+            })
+            .catch((error) => {
+              setErrorMessage(error.message);
+            });
+        }}
+      >
+        Change Password
       </button>
       {errorMessage && <p>{errorMessage}</p>}
     </div>
