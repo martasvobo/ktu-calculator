5733b18975c70921b17303eaa46f016160ad7c85
Slitchy20 <slitchy2@gmail.com>
2024-04-29 21:12:50 +0300

    Added styling to the topic page
    added rectangle windows to each comment to make them more visible to the eye.

--- src/TopicPage.jsx
+++ src/TopicPage.jsx
@@ -1,20 +1,15 @@
-import {
-  arrayUnion,
-  doc,
-  getDoc,
-  getFirestore,
-  onSnapshot,
-  updateDoc,
-} from "firebase/firestore";
 import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { UserContext } from "./App";
+import { arrayUnion, doc, getFirestore, onSnapshot, updateDoc } from "firebase/firestore";
+import "./TopicPage.css"; // Import the CSS file
 
 export default function TopicPage() {
   const user = useContext(UserContext);
   const { topicId } = useParams();
   const [topic, setTopic] = useState({});
   const [newComment, setNewComment] = useState("");
+
   useEffect(() => {
     return onSnapshot(doc(getFirestore(), "topics", topicId), (doc) => {
       setTopic(doc.data());
@@ -27,22 +22,29 @@ export default function TopicPage() {
       comments: arrayUnion({ text: newComment, user: user.email }),
     });
   };
+
   return (
-    <div>
-      <div>{topic.topic}</div>
-      <ul>
+    <div className="topic-page-container">
+      <div className="topic-header">{topic.topic}</div>
+      <ul className="comment-list">
         {topic.comments &&
           topic.comments.map((comment, index) => (
-            <li key={index}>{comment.text + " " + "(" + comment.user + ")"}</li>
+            <li key={index} className="comment-item">
+              <span className="comment-text">{comment.text}</span>
+              <span className="comment-user"> ({comment.user})</span>
+            </li>
           ))}
       </ul>
       <input
         type="text"
         value={newComment}
         onChange={(e) => setNewComment(e.target.value)}
+        className="comment-input"
         placeholder="Comment"
       />
-      <button onClick={addComment}>Comment</button>
+      <button onClick={addComment} className="comment-button">
+        Comment
+      </button>
     </div>
   );
 }
