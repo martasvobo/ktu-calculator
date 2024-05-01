70322e099013ef994a82946847f7ecb9e8595f0e
Slitchy20 <slitchy2@gmail.com>
2024-05-01 11:52:31 +0300

    added static code analyzer (Eslint)
    added a static code analyzer for testing

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   // Other Jest configuration options
-  testMatch: ["**/*.test.(js|jsx|ts|tsx)"],
+  testMatch: ['**/*.test.(js|jsx|ts|tsx)'],
   transform: {
-    "^.+\\.jsx?$": "babel-jest",
+    '^.+\\.jsx?$': 'babel-jest'
   },
   moduleNameMapper: {
-    "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js",
-  },
+    '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js'
+  }
 };
