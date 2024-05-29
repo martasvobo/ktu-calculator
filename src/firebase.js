2da8a08a4b3bd2bdfd5e3add039a5b0fe4f8f88c
martasvobo <62069655+martasvobo@users.noreply.github.com>
2024-05-29 17:42:33 +0300

    added performance monitoring

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,17 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getPerformance } from "firebase/performance";
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAHMKdvge9IbE5Q1mocLk9u_t9ZOtDAet8',
-  authDomain: 'ktu-calculator.firebaseapp.com',
-  projectId: 'ktu-calculator',
-  storageBucket: 'ktu-calculator.appspot.com',
-  messagingSenderId: '841434618819',
-  appId: '1:841434618819:web:73564581e91da712b03b76'
+  apiKey: "AIzaSyAHMKdvge9IbE5Q1mocLk9u_t9ZOtDAet8",
+  authDomain: "ktu-calculator.firebaseapp.com",
+  projectId: "ktu-calculator",
+  storageBucket: "ktu-calculator.appspot.com",
+  messagingSenderId: "841434618819",
+  appId: "1:841434618819:web:73564581e91da712b03b76",
 };
 
 const app = initializeApp(firebaseConfig);
+getPerformance(app);
 export default app;
 export const auth = getAuth(app);
